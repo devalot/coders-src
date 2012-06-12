409393ced1f3c53a8235ea14b77ef491dcb5f7df
Peter Jones <pjones@pmade.com>
2012-06-12 09:45:34 -0600

    Check for a tie game in rock paper scissors

--- ruby/rock_paper_scissors.rb
+++ ruby/rock_paper_scissors.rb
@@ -7,7 +7,9 @@ class RockPaperScissors
   }
 
   def play (hand_one, hand_two)
-    if WINS[hand_one] == hand_two
+    if hand_one == hand_two
+      "tie"
+    elsif WINS[hand_one] == hand_two
       hand_one
     else
       hand_two
--- ruby/test_rock_paper_scissors.rb
+++ ruby/test_rock_paper_scissors.rb
@@ -8,9 +8,14 @@ class TestRockPaperScissors < Test::Unit::TestCase
   ##############################################################################
   def test_rps
     rps = RockPaperScissors.new
+
     assert_equal("rock",     rps.play("rock", "scissors"))
     assert_equal("paper",    rps.play("paper", "rock"))
     assert_equal("scissors", rps.play("scissors", "paper"))
     assert_equal("paper",    rps.play("rock", "paper"))
+
+    assert_equal("tie", rps.play("rock", "rock"))
+    assert_equal("tie", rps.play("paper", "paper"))
+    assert_equal("tie", rps.play("scissors", "scissors"))
   end
 end
